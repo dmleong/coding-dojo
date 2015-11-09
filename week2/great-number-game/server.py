c4de632ff39c061f10bfffd633c4d9f7472441a1
Danielle Leong <danielle@danielleleong.com>
2015-11-09 15:37:12 -0800

    Great number game

--- week2/great-number-game/server.py
+++ week2/great-number-game/server.py
@@ -1,13 +1,48 @@
-from flask import Flask, render_template, session
+from flask import Flask, flash, render_template, request, redirect, session
 import random
 
 app = Flask(__name__)
 app.secret_key = 'ThisIsSecret'
 
-num = random.randrange(0, 101)
+def setSession():
+    session['num'] = random.randint(1,100)
 
 @app.route('/')
 def index():
+    if session['num'] != None:
+        pass
+    else:
+        setSession()
+    print session['num']
     return render_template('index.html')
+
+@app.route('/guess', methods=['POST'])
+def checkNumber():
+    error = None
+    success = None
+    guess = request.form['guess']
+    if request.method == 'POST':
+        if guess.isdigit():
+            numguess = int(guess)
+            if numguess == session['num']:
+                flash('Correct', 'success')
+                return redirect('/')
+            elif numguess > session['num']:
+                flash('Too high', 'error')
+            else:
+                flash('Too low', 'error')
+        else:
+            flash('Not a valid guess', 'error')
+    elif isinstance(guess, str):
+        flash('Not a valid guess', 'error')
+    else:
+        flash('Not a valid guess', 'error')
+
+    return redirect('/')
+
+@app.route('/reset', methods=['GET', 'POST'])
+def reset():
+    setSession()
+    return redirect('/')
 
 app.run(debug=True)
